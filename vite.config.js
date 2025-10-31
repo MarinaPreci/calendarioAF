f43c9696eb52eef93695008cd1fa94701f9509de
MarinaPreci <mpreciadoa02@educarex.es>
2025-10-31 11:36:46 +0100

    cambios vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/calendarioAF/' // <- el nombre de tu repositorio
+  base: '/' // <- el nombre de tu repositorio
 })
